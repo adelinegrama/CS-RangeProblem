222f1242fd4ee9edfd94e933d8cd1ded30add275
adelinegrama <grama.alina2003@gmail.com>
2024-09-07 17:05:39 +0300

    second commit

--- RangeProblem/Program.cs
+++ RangeProblem/Program.cs
@@ -45,58 +45,6 @@ namespace RangeProblem
             RangeProblemFunctions.RangeOutput(resultForLoop);
             RangeProblemFunctions.RangeOutput(resultWhileLoop);
             RangeProblemFunctions.RangeOutput(resultFillRangeList);
-
-
-            //Задание 59
-            //проверить, можно ли из заданной последовательности целых чисел 
-            //создать строго возрастающую последовательность в виде массива
-
-            //Задание 63
-            //check if a given number is present in an array of numbers
-
-            //Задание  65
-            //умножить все элементы заданного массива чисел на длину массива
-
-            //Задание 67
-            //create a coded string from a given string, using a specified formula.
-            //Replace all 'P' with '9', 'T' with '0', 'S' with '1', 'H' with '6' and 'A' with '8'.
-
-            //Задание 69
-            //check if a given string contains only lowercase or uppercase characters
-
-            //Задание 71
-            //given string contains two similar consecutive letters
-
-            //Задание 72
-            //check whether the average value of the elements of a given array of numbers is a whole number or not
-
-            //Задание 73
-            //convert the letters of a given string (same case-upper/lower) into alphabetical order
-
-            //Задание 74
-            //check the length of a given string is odd or even
-
-            //Задание 75
-            //takes a positive number and returns the nth odd number
-
-            //Задание 76
-            //get the ASCII value of a given character
-
-            //Задание 77
-            //check whether a word is plural or not
-
-            //Задание 82
-            //remove all non-letter characters from a given string
-
-            //Задание 83
-            //remove all vowels from a given string
-
-            //Задание 84
-            //get the index number of all lower case letters in a given string.
-
-            //Задание 86
-            //get the number of letters and digits in a given string
-
         }
     }
 }
